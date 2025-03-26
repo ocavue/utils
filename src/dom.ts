0a34a5a3a6f9eb6138c7e04de2e061d9805832eb
ocavue <ocavue@gmail.com>
2025-03-27 10:09:47 +1100

    chore: update build config

--- src/dom.ts
+++ src/dom.ts
@@ -1,3 +1,21 @@
+/**
+ * @internal
+ */
+enum NodeType {
+  ELEMENT_NODE = 1 satisfies typeof Node.ELEMENT_NODE,
+  ATTRIBUTE_NODE = 2 satisfies typeof Node.ATTRIBUTE_NODE,
+  TEXT_NODE = 3 satisfies typeof Node.TEXT_NODE,
+  CDATA_SECTION_NODE = 4 satisfies typeof Node.CDATA_SECTION_NODE,
+  ENTITY_REFERENCE_NODE = 5 satisfies typeof Node.ENTITY_REFERENCE_NODE,
+  ENTITY_NODE = 6 satisfies typeof Node.ENTITY_NODE,
+  PROCESSING_INSTRUCTION_NODE = 7 satisfies typeof Node.PROCESSING_INSTRUCTION_NODE,
+  COMMENT_NODE = 8 satisfies typeof Node.COMMENT_NODE,
+  DOCUMENT_NODE = 9 satisfies typeof Node.DOCUMENT_NODE,
+  DOCUMENT_TYPE_NODE = 10 satisfies typeof Node.DOCUMENT_TYPE_NODE,
+  DOCUMENT_FRAGMENT_NODE = 11 satisfies typeof Node.DOCUMENT_FRAGMENT_NODE,
+  NOTATION_NODE = 12 satisfies typeof Node.NOTATION_NODE,
+}
+
 export function isElement(node: Node): node is Element {
   return node.nodeType === NodeType.ELEMENT_NODE
 }
@@ -19,28 +37,4 @@ export function isMathMLElement(node: Node): node is MathMLElement {
     isElement(node) &&
     node.namespaceURI === 'http://www.w3.org/1998/Math/MathML'
   )
-}
-
-/**
- * @internal
- */
-enum NodeType {
-  ELEMENT_NODE = 1 satisfies typeof Node.ELEMENT_NODE,
-  ATTRIBUTE_NODE = 2 satisfies typeof Node.ATTRIBUTE_NODE,
-  TEXT_NODE = 3 satisfies typeof Node.TEXT_NODE,
-  CDATA_SECTION_NODE = 4 satisfies typeof Node.CDATA_SECTION_NODE,
-  ENTITY_REFERENCE_NODE = 5 satisfies typeof Node.ENTITY_REFERENCE_NODE,
-  ENTITY_NODE = 6 satisfies typeof Node.ENTITY_NODE,
-  PROCESSING_INSTRUCTION_NODE = 7 satisfies typeof Node.PROCESSING_INSTRUCTION_NODE,
-  COMMENT_NODE = 8 satisfies typeof Node.COMMENT_NODE,
-  DOCUMENT_NODE = 9 satisfies typeof Node.DOCUMENT_NODE,
-  DOCUMENT_TYPE_NODE = 10 satisfies typeof Node.DOCUMENT_TYPE_NODE,
-  DOCUMENT_FRAGMENT_NODE = 11 satisfies typeof Node.DOCUMENT_FRAGMENT_NODE,
-  NOTATION_NODE = 12 satisfies typeof Node.NOTATION_NODE,
-  DOCUMENT_POSITION_DISCONNECTED = 0x01 satisfies typeof Node.DOCUMENT_POSITION_DISCONNECTED,
-  DOCUMENT_POSITION_PRECEDING = 0x02 satisfies typeof Node.DOCUMENT_POSITION_PRECEDING,
-  DOCUMENT_POSITION_FOLLOWING = 0x04 satisfies typeof Node.DOCUMENT_POSITION_FOLLOWING,
-  DOCUMENT_POSITION_CONTAINS = 0x08 satisfies typeof Node.DOCUMENT_POSITION_CONTAINS,
-  DOCUMENT_POSITION_CONTAINED_BY = 0x10 satisfies typeof Node.DOCUMENT_POSITION_CONTAINED_BY,
-  DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC = 0x20 satisfies typeof Node.DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC,
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
+  tsconfig: 'tsconfig.build.json',
   clean: true,
-  dts: true,
+  experimentalDts: true,
 })
