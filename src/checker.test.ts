0d5095819b7e7188bda6468b78468dbb4e3c0084
ocavue <ocavue@gmail.com>
2025-07-24 15:40:13 +1000

    test

--- src/checker.test.ts
+++ src/checker.test.ts
@@ -22,9 +22,17 @@ describe('isObject', () => {
 
   it('returns false for primitives', () => {
     expect(isObject(42)).toBe(false)
-    expect(isObject('string')).toBe(false)
+    expect(isObject(42n)).toBe(false)
+    expect(isObject(Number.NaN)).toBe(false)
+    expect(isObject(Infinity)).toBe(false)
+    expect(isObject(null)).toBe(false)
+    expect(isObject(undefined)).toBe(false)
     expect(isObject(true)).toBe(false)
+    expect(isObject(false)).toBe(false)
+    expect(isObject(Symbol.iterator)).toBe(false)
+    expect(isObject(Symbol.for('foo'))).toBe(false)
     expect(isObject(Symbol())).toBe(false)
+    expect(isObject('string')).toBe(false)
   })
 
   it('returns true for class instances', () => {
