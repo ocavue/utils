2116b99ea8eba82e3d771ec6c7cec6fbfb0321ac
ocavue <ocavue@gmail.com>
2025-10-29 13:26:19 +1100

    use mo

---
+++ src/dom-node-type.ts
@@ -0,0 +1,13 @@
+
+    export const ELEMENT_NODE = 1 satisfies typeof Node.ELEMENT_NODE;
+    export const ATTRIBUTE_NODE = 2 satisfies typeof Node.ATTRIBUTE_NODE;
+    export const TEXT_NODE = 3 satisfies typeof Node.TEXT_NODE;
+    export const CDATA_SECTION_NODE = 4 satisfies typeof Node.CDATA_SECTION_NODE;
+    export const ENTITY_REFERENCE_NODE = 5 satisfies typeof Node.ENTITY_REFERENCE_NODE;
+    export const ENTITY_NODE = 6 satisfies typeof Node.ENTITY_NODE;
+    export const PROCESSING_INSTRUCTION_NODE = 7 satisfies typeof Node.PROCESSING_INSTRUCTION_NODE;
+    export const COMMENT_NODE = 8 satisfies typeof Node.COMMENT_NODE;
+    export const DOCUMENT_NODE = 9 satisfies typeof Node.DOCUMENT_NODE;
+    export const DOCUMENT_TYPE_NODE = 10 satisfies typeof Node.DOCUMENT_TYPE_NODE;
+    export const DOCUMENT_FRAGMENT_NODE = 11 satisfies typeof Node.DOCUMENT_FRAGMENT_NODE;
+    export const NOTATION_NODE = 12 satisfies typeof Node.NOTATION_NODE;
