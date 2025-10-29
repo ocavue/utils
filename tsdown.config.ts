f9c0fc9b3936790fdfb4cbcfa3ba45a5d2f2343f
ocavue <ocavue@gmail.com>
2025-10-29 13:19:54 +1100

    update tsconfig

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  tsconfig: 'tsconfig.build.json',
-  clean: true,
-  dts: {
-    compilerOptions: {
-      composite: false,
-    },
-  },
+  dts: true,
 })
