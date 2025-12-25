22019850b3d75b507c28503ba9fc6504807734e0
ocavue <ocavue@users.noreply.github.com>
2025-12-25 14:30:14 +1100

    fix: improve maximum safe integer limit (#50)

--- src/get-id.ts
+++ src/get-id.ts
@@ -1,9 +1,23 @@
 let id = 0
 
+let maxSafeInteger = Number.MAX_SAFE_INTEGER
+
+/**
+ * Sets the maximum safe integer for the id generator. Only for testing purposes.
+ *
+ * @internal
+ */
+export function setMaxSafeInteger(max: number) {
+  maxSafeInteger = max
+}
+
 /**
  * Generates a unique positive integer.
  */
 export function getId(): number {
-  id = (id % Number.MAX_SAFE_INTEGER) + 1
+  id++
+  if (id >= maxSafeInteger) {
+    id = 1
+  }
   return id
 }
