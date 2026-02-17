c98e4cf747485ddac237149e1342c894383b5295
ocavue <ocavue@gmail.com>
2026-02-17 20:22:28 +1100

    feat: add `canUseRegexLookbehind`

---
+++ src/regex.ts
@@ -0,0 +1,12 @@
+import { once } from './once'
+
+/**
+ * Checks if the browser supports [regex lookbehind assertion](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Lookbehind_assertion).
+ */
+export const canUseRegexLookbehind: () => boolean = /** @__PURE__ */ once(() => {
+  try {
+    return 'ab'.replace(new RegExp('(?<=a)b', 'g'), 'c') === 'ac'
+  } catch {
+    return false
+  }
+})
