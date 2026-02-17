af313ca658d1f755527f844c1b0ab46b516caf44
ocavue <ocavue@gmail.com>
2026-02-17 20:29:32 +1100

    fix

--- src/regex.ts
+++ src/regex.ts
@@ -3,7 +3,7 @@ import { once } from './once'
 /**
  * Checks if current environment supports [regex lookbehind assertion](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Lookbehind_assertion).
  */
-export const supportsRegexLookbehind: () => boolean = /** @__PURE__ */ once(
+export const supportsRegexLookbehind: () => boolean = /* @__PURE__ */ once(
   () => {
     try {
       return 'ab'.replace(new RegExp('(?<=a)b', 'g'), 'c') === 'ac'
